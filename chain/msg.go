3ab006fa52abe01c8c2b4de464f23054cdfd4603
jhue <threeax@foxmail.com>
2025-07-31 15:34:02 +0800

    feat: 升级zero bot v1.8.1

--- chain/msg.go
+++ chain/msg.go
@@ -6,35 +6,35 @@ import (
 )
 
 // MessageChain 消息链
-type MessageChain []message.MessageSegment
+type MessageChain []message.Segment
 
 var lb = message.Text("\n")
 var empty = message.Text(" ")
 
 // Join 加入一条消息
-func (c *MessageChain) Join(segment message.MessageSegment) MessageChain {
+func (c *MessageChain) Join(segment message.Segment) MessageChain {
 	*c = append(*c, segment)
 	return *c
 }
 
 // Line 加入一行消息，在末尾换行
-func (c *MessageChain) Line(segments ...message.MessageSegment) MessageChain {
+func (c *MessageChain) Line(segments ...message.Segment) MessageChain {
 	*c = append(*c, append(segments, lb)...)
 	return *c
 }
 
 // Split 分割消息，每条消息间会以换行分割
-func (c *MessageChain) Split(segments ...message.MessageSegment) MessageChain {
+func (c *MessageChain) Split(segments ...message.Segment) MessageChain {
 	return c.split(lb, segments...)
 }
 
 // SplitEmpty 分割消息，每条消息间会以空格分割
-func (c *MessageChain) SplitEmpty(segments ...message.MessageSegment) MessageChain {
+func (c *MessageChain) SplitEmpty(segments ...message.Segment) MessageChain {
 	return c.split(empty, segments...)
 }
 
 // split 分割消息
-func (c *MessageChain) split(sep message.MessageSegment, segments ...message.MessageSegment) MessageChain {
+func (c *MessageChain) split(sep message.Segment, segments ...message.Segment) MessageChain {
 	last := len(segments) - 1
 	for idx, segment := range segments {
 		if idx == last {
